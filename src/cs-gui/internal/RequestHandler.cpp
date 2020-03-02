46d3ab2d14efc4014e596a0ce232af3740cf4ea3
Jonas Gilg <jonas.gilg@dlr.de>
2020-03-02 12:07:44 +0100

    :wrench: Remove the default browser output when opening an external link.

--- src/cs-gui/internal/RequestHandler.cpp
+++ src/cs-gui/internal/RequestHandler.cpp
@@ -33,16 +33,17 @@ bool RequestHandler::OnBeforeBrowse(CefRefPtr<CefBrowser> browser, CefRefPtr<Cef
 
   // TODO it would be better to create a whitelist (or blacklist) of specific urls.
   if (utils::startsWith(url, "http") || utils::startsWith(url, "www")) {
+    spdlog::info("Opening the following URL in an external browser: {}", url);
 
     // Open links in the default OS browser to avoid breaking the CosmoScout GUI.
     if constexpr (utils::HostOS == utils::OS::Linux) {
-      std::string command = "xdg-open " + url;
+      std::string command = "xdg-open " + url + " >/dev/null 2>&1";
       system(command.c_str());
     } else if constexpr (utils::HostOS == utils::OS::Mac) {
-      std::string command = "open " + url;
+      std::string command = "open " + url + " >/dev/null 2>&1";
       system(command.c_str());
     } else if constexpr (utils::HostOS == utils::OS::Windows) {
-      std::string command = "start " + url;
+      std::string command = "start " + url + " > nul";
       system(command.c_str());
     }
 
